52c3564dd4e96fe5556acbdf918588dc4467720a
amanzano1 <amanzano1@cuc.edu.co>
2024-09-14 23:50:45 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import milogo from "./assets/img/LBK.png"
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={milogo} alt= "Escudo de los Kuervos" />
+      <h1>La gloriosa banda de junior</h1>
+      <p>Fundada en 2006, 18 años de aguante y korazon</p>
+    </div>
+    </>
+  )
+}
+
+export default App
